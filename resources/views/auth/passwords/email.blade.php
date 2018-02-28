184e9b613273d70f6300890fe7f67f8c791b9c94
astalpaert <arno.stalpaert@hotmail.com>
2018-02-28 20:45:15 +0100

    Views
    Error view
    Pass reset, Pass mail

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,60 @@
-@extends('layouts.app')
+@extends('layouts.website')
 
 @section('content')
+
+
+    <div id="login_page">
+        <div class="login_box">
+
+            <a href="{{route('website-index')}}"><img src="{{ asset('img/logo/Logo_final.png') }}"></a>
+
+
+
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <form method="POST" action="{{ route('password.email') }}">
+
+                {{ csrf_field() }}
+
+
+
+
+                <div class="form-group clearfix col-xs-12">
+                    <label for="email">E-mailadres</label>
+                    <input type="email" class="form-control col-lg-6" id="email" name="email" aria-describedby="usernameHelp" value="{{ old('email') }}" required>
+                    @if ($errors->has('email'))
+                        <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+                    @endif
+                </div>
+
+
+
+
+
+
+
+
+
+                <div class="form-group clearfix col-xs-12">
+                    <label class="form-check-label">
+                        <input type="checkbox" name="remember" class="form-check-input">Dit is mijn account
+                    </label>
+                </div>
+                <button type="submit" class="action-button login">Wachtwoord reset aanvragen</button>
+            </form>
+
+            <a class="login-problem" href="{{ route('login') }}"><p>Terug naar inloggen.</p></a>
+        </div>
+    </div>
+
+
+
+
+<!--
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -43,5 +97,6 @@
             </div>
         </div>
     </div>
+    -->
 </div>
 @endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,20 @@
-Hier komt de 404 pagina
+@extends('layouts.website')
+
+@section('content')
+    <div id="login_page">
+        <div class="login_box">
+
+            <a href="{{route('website-index')}}"><img src="{{ asset('img/logo/Logo_final.png') }}"></a>
+
+            <div class="page-error">
+                <h1 class="">404</h1>
+                <br>
+                <h4>Deze pagina kon niet gevonden worden...</h4>
+                <br>
+            </div>
+
+
+            <a class="login-problem" href="{{ route('website-index') }}"><p>Ga naar de home pagina.</p></a>
+        </div>
+    </div>
+@endsection
