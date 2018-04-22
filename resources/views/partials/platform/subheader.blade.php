7512d73ed87a0d660e418345a708d90b244f21f3
Jonas <defrere.jonas@gmail.com>
2018-04-22 21:55:07 +0200

    Use tutoringCount() function as global in subheader

--- resources/views/partials/platform/subheader.blade.php
+++ resources/views/partials/platform/subheader.blade.php
@@ -4,7 +4,7 @@
             <li class="{{ ActiveHelper::getState(route('community-index', [], false)) }}"><a href="{{ route("community-index") }}"><span><i class="fa fa-users" aria-hidden="true"></i></span>Community</a></li>
             <li class="{{ ActiveHelper::getState(route('sharing-index', [], false)) }}"><a href="{{ route("sharing-index") }}"><span><i class="fa fa-file" aria-hidden="true"></i></span>Bestanden</a></li>
             <li><a href="#"><span><i class="fa fa-clipboard" aria-hidden="true"></i></span>Peer Assessment</a></li>
-            <li class="{{ ActiveHelper::getState(route('tutoring-index', [], false)) }}"><a href="{{ route("tutoring-index") }}"><span><i class="fa fa-life-ring" aria-hidden="true"></i></span>Tutoring<span style="margin-top: -20px; font-size: 10px" class="badge">3</span></a></li>
+            <li class="{{ ActiveHelper::getState(route('tutoring-index', [], false)) }}"><a href="{{ route("tutoring-index") }}"><span><i class="fa fa-life-ring" aria-hidden="true"></i></span>Tutoring<span style="margin-top: -20px; font-size: 10px" class="badge">{{ Variable::tutoringCount() }}</span></a></li>
         </ul>
     </div>
 </nav>
