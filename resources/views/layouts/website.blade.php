c477688c72b95d18b0b8c575c1dfbafdefa23d9d
astalpaert <arno.stalpaert@hotmail.com>
2018-05-01 11:34:45 +0200

    Nav fix + hotjar script

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -2,29 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
     <!-- mobile responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>UniHelp | @yield('pagetitle')</title>
-
-
     <!-- favicon -->
     <link rel="shortcut icon" type="image/png" href="{{asset('img/logo/favicon.png')}}"/>
-
     <!-- bootstrap -->
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-theme.css') }}">
-
     <!-- own css -->
     <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/inputs.css') }}">
-
+    <!-- Slicknav -->
     <link rel="stylesheet" href="{{ URL::asset('css/slicknav.css') }}">
-
     <!-- mailchimp -->
     <link rel="stylesheet" href="{{ URL::asset('css/mailchimp-horizontal-slim-10_7.css') }}">
-
     <!-- fontawesome -->
     <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.css') }}">
 
@@ -59,7 +51,6 @@
         });
     });
 </script>
-
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-47689548-7"></script>
 <script>
@@ -70,10 +61,18 @@
     }
 
     gtag('js', new Date());
-
     gtag('config', 'UA-47689548-7');
 </script>
-
-
+<!-- Hotjar Tracking Code for https://unihelp.be/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:737039,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </body>
 </html>
