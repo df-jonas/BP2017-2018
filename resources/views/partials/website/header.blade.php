b39398f3facd54a9d1c28d9a0062615ba1a28b88
Jonas <defrere.jonas@gmail.com>
2018-05-15 19:41:13 +0200

    fix

--- resources/views/partials/website/header.blade.php
+++ resources/views/partials/website/header.blade.php
@@ -6,31 +6,17 @@
                     <a href="{{ route('website-index') }}"><img class="logo" src="img/logo/favicon.png"></a>
                 </div>
                 <ul id="menu" class="links col-lg-8 col-sm-10 col-xs-12">
-                    <a href="{{ route('website-index') }}">
-                        <li>Home</li>
-                    </a>
-                    <a href="{{ route('website-about') }}">
-                        <li>Over</li>
-                    </a>
-                    <a href="{{ route('website-how') }}">
-                        <li>Hoe het werkt</li>
-                    </a>
+                    <a class="{{ ActiveHelper::getState(route('website-index', [], false), "active-state", "nonactive-state") }}" href="{{ route('website-index') }}"><li>Home</li></a>
+                    <a class="{{ ActiveHelper::getState(route('website-about', [], false), "active-state", "nonactive-state") }}" href="{{ route('website-about') }}"><li>Over</li></a>
+                    <a class="{{ ActiveHelper::getState(route('website-how', [], false), "active-state", "nonactive-state") }}" href="{{ route('website-how') }}"><li>Hoe het werkt</li></a>
                     @if (Auth::check())
-                        <a href="{{ route('login') }}">
-                            <li class="register">Platform</li>
-                        </a>
+                        <a href="{{ route('login') }}"><li class="register">Platform</li></a>
                     @else
-                        <a href="{{ route('login') }}">
-                            <li>Inloggen</li>
-                        </a>
-                        <a href="{{ route('register') }}">
-                            <li class="register">Registreren</li>
-                        </a>
+                        <a href="{{ route('login') }}"><li>Inloggen</li></a>
+                        <a href="{{ route('register') }}"><li class="register">Registreren</li></a>
                     @endif
                 </ul>
             </div>
         </div>
     </div>
-
 </header>
-
